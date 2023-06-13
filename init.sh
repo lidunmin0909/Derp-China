4ee8c45a1f70a2849d3e42cc68a864b7502f26aa
s4kur4 <s4kur4s4kur4@gmail.com>
2023-06-13 19:58:38 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,11 +4,5 @@
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state >> /dev/stdout &
 /usr/bin/tailscale up --accept-routes=true --accept-dns=true --auth-key $TAILSCALE_AUTH_KEY >> /dev/stdout &
 
-#Check for and or create certs directory
-if [[ ! -d "/root/derper/$TAILSCALE_DERP_HOSTNAME" ]]
-then
-    mkdir -p /root/derper/$TAILSCALE_DERP_HOSTNAME
-fi
-
 #Start Tailscale derp server
 /root/go/bin/derper --hostname=$TAILSCALE_DERP_HOSTNAME --a=$TAILSCALE_DERP_ADDR --stun-port=$TAILSCALE_DERP_STUN_PORT --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
