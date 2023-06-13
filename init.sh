42e3a9bbc62b503d6724a6538c86e0bf06238a5d
s4kur4 <s4kur4s4kur4@gmail.com>
2023-06-13 20:26:57 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 #Start tailscaled and connect to tailnet
-/usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state >> /dev/stdout &
-/usr/bin/tailscale up --accept-routes=true --accept-dns=true --auth-key $TAILSCALE_AUTH_KEY >> /dev/stdout &
+/usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state &> /var/lib/tailscale/tailscaled_initial.log &
+/usr/bin/tailscale up --accept-routes=true --accept-dns=true --auth-key $TAILSCALE_AUTH_KEY &> /var/lib/tailscale/tailscale_onboard.log &
 
 #Start Tailscale derp server
 /root/go/bin/derper --hostname=$TAILSCALE_DERP_HOSTNAME --a=$TAILSCALE_DERP_ADDR --stun-port=$TAILSCALE_DERP_STUN_PORT --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
